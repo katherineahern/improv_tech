25468b7b26e2a69d4e2e4d773c178e6504ea38bd
katherineahern <kate@emergentdatasystems.com>
2018-03-15 11:07:33 -0700

    Working prototype plus placeholder mp3s

--- musical_accompaniment/my-app/src/App.js
+++ musical_accompaniment/my-app/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sound from 'react-sound';
 import './App.css';
+import happy from './songs/happy.mp3';
+import sad from './songs/sad.mp3';
+import angry from './songs/angry.mp3';
+import fearful from './songs/fearful.mp3';
 
 class App extends Component {
   render() {
@@ -20,9 +24,46 @@ class App extends Component {
 }
 
 function Emotion(props) {
+  
   return (
-    <div className="col">{props.emotionName}</div>
+    <div className="col">
+      {props.emotionName}<br/>
+      <audio controls>
+        <Song emotion={props.emotionName.toLowerCase()} />
+      </audio>
+    </div>
   );
+}
+
+function Song(props) {
+  
+  switch(props.emotion) {
+    case 'happy':
+      return (
+        <source src={happy} type="audio/mpeg" />
+      );
+      break;
+    case 'sad':
+      return (
+        <source src={sad} type="audio/mpeg" />
+      );
+      break;
+    case 'angry':
+      return (
+        <source src={angry} type="audio/mpeg" />
+      );
+      break;
+    case 'fearful':
+      return (
+        <source src={fearful} type="audio/mpeg" />
+      );
+      break;
+    default:
+      return (
+        <source src={happy} type="audio/mpeg" />
+      );
+  }
+  
 }
 
 export default App;

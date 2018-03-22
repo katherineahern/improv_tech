22fa1389ff0c381bd7bb12e9fdf3b00aefe44df7
katherineahern <kate@emergentdatasystems.com>
2018-03-21 20:56:29 -0700

    State refinements

--- musical_accompaniment/my-app/src/Song.js
+++ musical_accompaniment/my-app/src/Song.js
@@ -5,45 +5,41 @@ import sad from './songs/sad.m4a';
 import angry from './songs/angry.m4a';
 import fearful from './songs/fearful.m4a';
 
+import funk_happy from './songs/funk_happy.m4a';
+import jazz_happy from './songs/jazz_happy.m4a';
+import latin_happy from './songs/latin_happy.m4a';
+import rock_happy from './songs/rock_happy.m4a';
+
+import funk_sad from './songs/funk_sad.m4a';
+import jazz_sad from './songs/jazz_sad.m4a';
+import latin_sad from './songs/latin_sad.m4a';
+import rock_sad from './songs/rock_sad.m4a';
+
 class Song extends React.Component {
 	constructor(props) {
-
 		super(props);
-		
+		this.state = {palette: this.props.palette};
+		this.getSongSrc = this.getSongSrc.bind(this);	
 	}
-
-
-	
-	 render() { 
-	 	console.log(this);
-	  	switch(this.props.emotion) {
-		    case 'happy':
-		      return (
-		        <source src={happy} type="audio/mpeg" />
-		      );
-		      break;
-		    case 'sad':
-		      return (
-		        <source src={sad} type="audio/mpeg" />
-		      );
-		      break;
-		    case 'angry':
-		      return (
-		        <source src={angry} type="audio/mpeg" />
-		      );
-		      break;
-		    case 'fearful':
-		      return (
-		        <source src={fearful} type="audio/mpeg" />
-		      );
-		      break;
-		    default:
-		      return (
-		        <source src={happy} type="audio/mpeg" />
-		      );
-		}
+	getSongSrc() {
+		console.log(this.props);
+	 	var songSelection = {
+			"happy": { "none": [rock_happy], "funk": [funk_happy], "jazz": [jazz_happy], "latin": [latin_happy] },
+			"sad": {  "none": [rock_sad], "funk": [funk_sad], "jazz": [jazz_sad], "latin": [latin_sad] },
+			//redo angry and fearful
+			"angry": { "none": [rock_happy], "funk": [funk_happy], "jazz": [jazz_happy], "latin": [latin_happy] },
+			"fearful": {  "none": [rock_sad], "funk": [funk_sad], "jazz": [jazz_sad], "latin": [latin_sad] }
+		};
+	 	console.log(songSelection[this.props.emotion][this.props.palette][0]);
+	 	return songSelection[this.props.emotion][this.state.palette][0];
 	 }
- 
+
+	render() { 
+	 	return (
+	    	<source src={this.getSongSrc()} type="audio/mpeg" />
+	    );
+	  	
+	 }
 }
 
 // Must export!

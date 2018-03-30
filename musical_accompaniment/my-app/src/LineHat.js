3527c25062265d4a5063670a2f923aed77807ebf
katherineahern <kate@emergentdatasystems.com>
2018-03-30 15:06:04 -0700

    Request from LineHat

--- musical_accompaniment/my-app/src/LineHat.js
+++ musical_accompaniment/my-app/src/LineHat.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class LineHat extends Component {
-  render() {
-    return (
-      <h1>Lines from a hat!</h1>
-    );
-  }
+
+	constructor () {
+		super();
+		this.state = {
+	    	response: {}
+	  	}
+		this.handleClick = this.handleClick.bind(this)
+	}
+  	render() {
+    	return (
+      		<div className='button__container'>
+      			<h1>Lines from a hat</h1>
+      			<p>Username from maecapozzi</p>
+		      	<button className='button' onClick={this.handleClick}>
+		        	Click Me
+		      	</button>
+		      	<p>{JSON.stringify(this.state)}</p>
+		      	
+		    </div>
+    	);
+  	}
+
+  	handleClick () {
+  		let config = {
+		  headers: {
+		    	"Content-Type": "application/x-www-form-urlencoded"
+		}
+}
+  		axios.get('http://ec2-54-237-240-235.compute-1.amazonaws.com:8080/api/lines', {}, config)
+    		.then(response => this.setState({response: response }))
+
+    		
+  	}
+
 }
 
 export default LineHat;
--- rest_service/server.js
+++ rest_service/server.js
@@ -3,7 +3,11 @@
 // BASE SETUP
 // =============================================================================
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://Katherines-MBP.attlocal.net:27017/line_hat'); // connect to our database
+//for running locally
+//mongoose.connect('mongodb://Katherines-MBP.attlocal.net:27017/line_hat'); // connect to our database
+
+//for running on AWS
+mongoose.connect('mongodb://localhost:27017/line_hat'); // connect to our database
 
 var LineHat     = require('./app/models/lineHat');
 
@@ -16,6 +20,25 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 var port = process.env.PORT || 8080;        // set our port
 

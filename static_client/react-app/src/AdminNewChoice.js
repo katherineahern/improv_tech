554a2d00b5432037f1b9d16e861e76643fdacb8e
katherineahern <kate@emergentdatasystems.com>
2018-04-11 13:11:39 -0700

    Get new choice message, play it

--- static_client/react-app/src/AdminNewChoice.js
+++ static_client/react-app/src/AdminNewChoice.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import new_choice from './songs/newChoice.mp3';
 
 var Socket;
 class AdminNewChoice extends Component {
 	constructor() {
 		super();
+		this.newChoice = this.newChoice.bind(this);
 	}
 	componentDidMount() {
 	    Socket = new WebSocket('ws://localhost:4444/' );
 	    Socket.onmessage = function(e) {
 	      console.log("got new choice");
 	      console.log(e.data);
+	      this.newChoice();
 	    }.bind(this); // you need to bind here
+  	}
+  	newChoice() {
+  		console.log("New Choice");
+  		var audio = this.refs['audio'];
+	    audio.load();
+	    audio.currentTime = 0;
+	    audio.play();
   	}
 
 	render() {
 		return (
 			<div>
 				<h1>Admin New Choice</h1>
+				<audio ref='audio' key={ Math.random() } > //key to rerender audio
+            		<source src={new_choice} type="audio/mpeg" />
+          		</audio>
 			</div>
 		);
 	}

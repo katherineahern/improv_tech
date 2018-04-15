e4ffefe000e5473a2e91902deb5e78956d5e33c8
katherineahern <kate@emergentdatasystems.com>
2018-04-15 14:51:48 -0700

    Using Google tts, fixed spell-it-out bug

--- scraper_tts/tts_test.py
+++ scraper_tts/tts_test.py
@@ -4,17 +4,60 @@ import sys
 import requests
 import re
 from bs4 import BeautifulSoup
+from gtts import gTTS
+import os    
+
+
 
 #url = raw_input("Enter a website to extract the URL's from: ")
 
-r  = requests.get("https://en.wikipedia.org/wiki/George_Washington")
+r  = requests.get("https://en.wikipedia.org/wiki/Adlai_Stevenson_II")
 
 data = r.text
 
 soup = BeautifulSoup(data, "html.parser")
 
 body_div = soup.find('div', id='bodyContent')
+nums = re.compile("[12][0-9]{3}")
+period = re.compile("\.")
+counter = 1
 
-for paragraph in soup.find_all('p'):
-    print(paragraph.get_text())
-    os.system('say ' + re.sub(r'\W+', '', paragraph.get_text()))
+def clean(string) :
+    cleaned1 = re.sub(r'\W+', '', string)
+    #cleaned2 = re.sub('[\[[0-9]?\]]', '', cleaned1)
+    cleaned3 = re.sub("'", '', cleaned1)
+    return re.sub(r'[\W ]+', '', string)
+
+def say(string) :
+	
+	try:
+		tts = gTTS(text=string, lang='en')
+		tts.save(str(counter) + ".mp3")
+		os.system("afplay " + str(counter) + ".mp3")
+	except :
+		print("error")
+	return
+
+for paragraph in body_div.find_all('p'):
+	candidates = paragraph.get_text().split(".")
+	for candidate in candidates:
+		print(clean(candidate))
+		#os.system('say --interactive=/green ' + clean(candidate))
+		say(clean(candidate))
+		counter = counter + 1
+    	variable = raw_input('input something!: ')
+
+    #if not nums.search(candidate) : 
+        
+        #os.system('say ' + re.sub(r'\W+', '', paragraph.get_text()))
+        #if counter > 3 and counter < 5 :
+        
+        #if candidate.isalpha() or candidate.isspace() :
+            #print(candidate)
+            #os.system('say --interactive=/green ' + candidate + '. /')
+            #os.system('say --interactive=/green -v Agnes ' + candidate + '.[[slnc 100]] ')
+            #os.system('say --interactive=/green hi there you. ')
+            #os.system('say --interactive=/green -v Zarvox "hi there are jay we can try with a longer sentence."')
+            
+        
+        counter = counter + 1

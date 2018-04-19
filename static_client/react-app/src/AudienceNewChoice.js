6d199699042d9dfceb7d1493b710ef590ee0571d
vaqueropants <adrianjoel.vazquez@gmail.com>
2018-04-19 10:44:26 -0700

    styling cleanup

--- static_client/react-app/src/AudienceNewChoice.js
+++ static_client/react-app/src/AudienceNewChoice.js
@@ -15,8 +15,16 @@ class AudienceNewChoice extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Audience New Choice</h1>
-				<button onClick={this.newChoice}>New Choice</button>
+   			<div className="header-spacer"></div>
+  			<header className="choice-header">
+	  			<h1 className="App-title">New Choice!</h1>
+  			</header>
+
+				<h2>You control the scene:</h2>
+				<p>When the actor says something you want them to change, tap the "New Choice" button.</p>
+				<p>They will come up with a new line and continue from there as though the last line never happened.</p>
+				<p>...</p>
+				<button onClick={this.newChoice}>📣 - New Choice! - ⏪</button>
 			</div>
 		);
 	}

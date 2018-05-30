f96fa27538640f2049422e3b6122e7a4e0d5f0ff
katherineahern <kate@emergentdatasystems.com>
2018-05-30 10:52:11 -0700

    Conditional render banner

--- static_client/react-app/src/Header.jsx
+++ static_client/react-app/src/Header.jsx
@@ -4,9 +4,29 @@ import * as Constants from './Constants';
 class Header extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+	    	heartbeat: false
+	    };
+		this.renderBanner = this.renderBanner.bind(this);
+	}
+	renderBanner() {
+		const red = "red";
+		if(!this.state.heartbeat) {
+			return (
+				<div style={{background: red}}>
+		       		<br/>
+		            This will only function during next show<br/>
+		            <a href="https://www.eventbrite.com/o/improvisational-technology-17153874424">Get tickets!</a>
+	          	</div>
+			);
+		} else {
+			return null;
+		}
 	}
 	render() {
-		const red = "red";
+		
+		
+		
 
 		return (
 			<div>
@@ -14,11 +34,8 @@ class Header extends Component {
 		          	<a class="applogo" href="App">⬅️</a>
 		           	<h1 className="App-title" id={this.props.headerId}>{this.props.title}</h1>
 		        </header>
-		       	<div style={{background: red}}>
-		       		<br/>
-		            This will only function during next show<br/>
-		            <a href="https://www.eventbrite.com/o/improvisational-technology-17153874424">Get tickets!</a>
-	          	</div>
+		        {this.renderBanner()}
+		       	
 	        </div>
 	    );
     }
